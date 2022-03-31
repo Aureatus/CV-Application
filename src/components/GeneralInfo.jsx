f76e1fc68a75541da49650eed2b9acf6829a0c0f
Alex Knott <alextknott@gmail.com>
2022-03-31 15:12:30 +0100

    Add text to labels relating to their className

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -8,16 +8,19 @@ class GeneralInfo extends Component {
         <main>
           <div className="name">
             <label htmlFor="name-input">
+              Name
               <input id="name-input" type="text" />
             </label>
           </div>
           <div className="email">
             <label htmlFor="email-input">
+              Email
               <input id="email-input" type="email" />
             </label>
           </div>
           <div className="phone-number">
             <label htmlFor="phone-number-input">
+              Phone Number
               <input id="phone-number-input" type="tel" />
             </label>
           </div>

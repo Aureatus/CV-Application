e44ba603a1a24ab53cefed9adf4de5474e83f4f2
Alex Knott <alextknott@gmail.com>
2022-04-01 11:14:10 +0100

    Declare state on GeneralInfo, add function that dynamically changes state on input, add function for changing inputs to static elements, and implement name section of form in it's own component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,18 +1,65 @@
 import React, { Component } from "react";
 
+function NameComponent(props) {
+  const { submitted } = props;
+  if (submitted === true) {
+    return (
+      <div className="name">
+        <label htmlFor="name-input">
+          Name
+          <input id="name-input" type="text" />
+        </label>
+      </div>
+    );
+  }
+}
+
 class GeneralInfo extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      submitted: true,
+    };
+
+    this.updateState = (e) => {
+      switch (e.target.id) {
+        case "name-input":
+          this.setState({ name: e.target.value }, () => {
+            console.log(this.state);
+          });
+          break;
+        case "email-input":
+          this.setState({ email: e.target.value }, () => {
+            console.log(this.state);
+          });
+          break;
+        case "phone-number-input":
+          this.setState({ phone: e.target.value }, () => {
+            console.log(this.state);
+          });
+          break;
+        default:
+          break;
+      }
+    };
+
+    this.inputToStatic = (e) => {
+      e.preventDefault();
+    };
+  }
+
   render() {
+    const { submitted } = this.state;
     return (
       <div className="general-info">
         <header>General Information</header>
         <main>
-          <form>
-            <div className="name">
-              <label htmlFor="name-input">
-                Name
-                <input id="name-input" type="text" />
-              </label>
-            </div>
+          <form onSubmit={this.inputToStatic} onChange={this.updateState}>
+            <NameComponent submitted={submitted} />
             <div className="email">
               <label htmlFor="email-input">
                 Email

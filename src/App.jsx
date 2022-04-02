2c6f8702c9058ddf6a87ff6c4d5f16f1ac60a443
Alex Knott <alextknott@gmail.com>
2022-04-02 17:11:00 +0100

    Import EducationalInfo component and return in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import GeneralInfo from "./components/GeneralInfo";
+import EducationalInfo from "./components/EducationalInfo";
 
 class App extends Component {
   render() {
     return (
       <div>
         <GeneralInfo />
+        <EducationalInfo />
       </div>
     );
   }

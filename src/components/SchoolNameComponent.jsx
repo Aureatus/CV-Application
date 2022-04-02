7152afb1626437c6da915300b9d5f940001a4bc2
Alex Knott <alextknott@gmail.com>
2022-04-02 17:23:48 +0100

    Capitalised start of each Component used in EducationalInfo

---
+++ src/components/SchoolNameComponent.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+
+class SchoolNameComponent extends Component {
+  render() {
+    const { submitted } = this.props;
+    const { name } = this.props;
+    const { updateStateNameEmailPhone } = this.props;
+    const { nameEditActive } = this.props;
+    const { nameEditActiveToggle } = this.props;
+    const inputOrStatic = () => {
+      if (submitted === false) {
+        return (
+          <div className="name">
+            <label htmlFor="name-input">
+              Name
+              <input id="name-input" type="text" />
+            </label>
+          </div>
+        );
+      }
+
+      if (submitted === true) {
+        if (nameEditActive === false) {
+          return (
+            <div className="name">
+              <label htmlFor="name-input">
+                Name
+                <p>{name}</p>
+                <button type="button" onClick={nameEditActiveToggle}>
+                  Edit
+                </button>
+              </label>
+            </div>
+          );
+        }
+        if (nameEditActive === true) {
+          return (
+            <div className="name">
+              <label htmlFor="name-input">
+                Name
+                <input id="name-input" type="text" defaultValue={name} />
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    nameEditActiveToggle();
+                    updateStateNameEmailPhone(e);
+                  }}
+                >
+                  Submit
+                </button>
+              </label>
+            </div>
+          );
+        }
+      }
+      return false;
+    };
+
+    return <div>{inputOrStatic()}</div>;
+  }
+}
+
+export default SchoolNameComponent;

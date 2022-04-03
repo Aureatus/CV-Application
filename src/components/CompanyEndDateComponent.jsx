a39a603df959ce8671517316979dd126de414857
Alex Knott <alextknott@gmail.com>
2022-04-03 17:34:04 +0100

    Add functionality to edit and resubmit CompanyEndDateComponent

---
+++ src/components/CompanyEndDateComponent.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+
+class CompanyEndDateComponent extends Component {
+  render() {
+    const { submitted } = this.props;
+    const { companyEndDate } = this.props;
+    const { updateStateNameTitleTasksDate } = this.props;
+    const { companyEndDateEditActive } = this.props;
+    const { companyEndDateEditActiveToggle } = this.props;
+    const inputOrStatic = () => {
+      if (submitted === false) {
+        return (
+          <div className="company-end-date">
+            <label htmlFor="company-end-date-input">
+              Company End Date
+              <input id="company-end-date-input" type="text" />
+            </label>
+          </div>
+        );
+      }
+
+      if (submitted === true) {
+        if (companyEndDateEditActive === false) {
+          return (
+            <div className="company-end-date">
+              <label htmlFor="company-end-date-input">
+                Company End Date
+                <p>{companyEndDate}</p>
+                <button type="button" onClick={companyEndDateEditActiveToggle}>
+                  Edit
+                </button>
+              </label>
+            </div>
+          );
+        }
+        if (companyEndDateEditActive === true) {
+          return (
+            <div className="company-end-date">
+              <label htmlFor="company-end-date-input">
+                Company End Date
+                <input
+                  id="company-end-date-input"
+                  type="text"
+                  defaultValue={companyEndDate}
+                />
+                <button
+                  type="button"
+                  onClick={(e) => {
+                    companyEndDateEditActiveToggle();
+                    updateStateNameTitleTasksDate(e);
+                  }}
+                >
+                  Submit
+                </button>
+              </label>
+            </div>
+          );
+        }
+      }
+      return false;
+    };
+
+    return <div>{inputOrStatic()}</div>;
+  }
+}
+
+export default CompanyEndDateComponent;

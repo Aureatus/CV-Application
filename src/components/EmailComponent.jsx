ba93658fd3789158858981ad91b546de8c221242
Alex Knott <alextknott@gmail.com>
2022-04-06 21:51:39 +0100

    Make EmailComponent a functional component

--- src/components/EmailComponent.jsx
+++ src/components/EmailComponent.jsx
@@ -1,70 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 
-class EmailComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { email } = this.props;
-    const { updateStateNameEmailPhone } = this.props;
-    const { emailEditActive } = this.props;
-    const { emailEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function EmailComponent(props) {
+  const { submitted } = props;
+  const { email } = props;
+  const { updateStateNameEmailPhone } = props;
+  const { emailEditActive } = props;
+  const { emailEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="email">
+          <label htmlFor="email-input">
+            Email
+            <input id="email-input" type="email" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (emailEditActive === false) {
         return (
           <div className="email">
             <label htmlFor="email-input">
               Email
-              <input id="email-input" type="email" />
+              <p>{email}</p>
+              <button type="button" onClick={emailEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (emailEditActive === false) {
-          return (
-            <div className="email">
-              <label htmlFor="email-input">
-                Email
-                <p>{email}</p>
-                <button type="button" onClick={emailEditActiveToggle}>
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (emailEditActive === true) {
-          return (
-            <div className="email">
-              <label htmlFor="email-input">
-                Email
-                <input
-                  id="email-input"
-                  type="email"
-                  defaultValue={email}
-                  required
-                />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    const emailInput = e.target.parentElement.children[0];
-                    if (emailInput.validity.valid) {
-                      emailEditActiveToggle();
-                      updateStateNameEmailPhone(e);
-                    }
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (emailEditActive === true) {
+        return (
+          <div className="email">
+            <label htmlFor="email-input">
+              Email
+              <input
+                id="email-input"
+                type="email"
+                defaultValue={email}
+                required
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  const emailInput = e.target.parentElement.children[0];
+                  if (emailInput.validity.valid) {
+                    emailEditActiveToggle();
+                    updateStateNameEmailPhone(e);
+                  }
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
-    return <div>{inputOrStatic()}</div>;
-  }
+    }
+    return false;
+  };
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default EmailComponent;

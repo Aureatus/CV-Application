bc9cc8966ed0bc17bb21b5ead64e4cbb3231e112
Alex Knott <alextknott@gmail.com>
2022-04-06 21:28:14 +0100

    Make GeneralInfo a functional Component

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,162 +1,116 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NameComponent from "./NameComponent";
 import EmailComponent from "./EmailComponent";
 import PhoneComponent from "./PhoneComponent";
 import SubmitComponent from "./SubmitComponent";
 import "../styles/info.css";
 
-class GeneralInfo extends Component {
-  constructor() {
-    super();
+function GeneralInfo() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
 
-    this.state = {
-      name: "",
-      email: "",
-      phone: "",
-      submitted: false,
-      editActive: {
-        nameEditActive: false,
-        emailEditActive: false,
-        phoneEditActive: false,
-      },
-    };
+  const [submitted, setSubmitted] = useState(false);
 
-    this.updateStateNameEmailPhone = (e) => {
-      if (e.target.validity.valid === false) {
-        return;
-      }
-      switch (e.target.id) {
-        case "name-input":
-          this.setState({ name: e.target.value });
-          break;
-        case "email-input":
-          this.setState({ email: e.target.value });
-          break;
-        case "phone-number-input":
-          this.setState({ phone: e.target.value });
-          break;
-        default:
-          break;
-      }
-    };
+  const [nameEditActive, setNameEditActive] = useState(false);
+  const [emailEditActive, setEmailEditActive] = useState(false);
+  const [phoneEditActive, setPhoneEditActive] = useState(false);
 
-    this.nameEditActiveToggle = () => {
-      const { editActive } = this.state;
-      const { nameEditActive } = editActive;
-      if (nameEditActive === false) {
-        this.setState({
-          editActive: {
-            nameEditActive: true,
-            emailEditActive: false,
-            phoneEditActive: false,
-          },
-        });
-      }
-      if (nameEditActive === true) {
-        this.setState({
-          editActive: {
-            nameEditActive: false,
-            emailEditActive: false,
-            phoneEditActive: false,
-          },
-        });
-      }
-    };
+  const updateStateNameEmailPhone = (e) => {
+    if (e.target.validity.valid === false) {
+      return;
+    }
+    switch (e.target.id) {
+      case "name-input":
+        setName(e.target.value);
+        break;
+      case "email-input":
+        setEmail(e.target.value);
+        break;
+      case "phone-number-input":
+        setPhone(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
 
-    this.emailEditActiveToggle = () => {
-      const { editActive } = this.state;
-      const { emailEditActive } = editActive;
-      if (emailEditActive === false) {
-        this.setState({
-          editActive: {
-            nameEditActive: false,
-            emailEditActive: true,
-            phoneEditActive: false,
-          },
-        });
-      }
-      if (emailEditActive === true) {
-        this.setState({
-          editActive: {
-            nameEditActive: false,
-            emailEditActive: false,
-            phoneEditActive: false,
-          },
-        });
-      }
-    };
+  const nameEditActiveToggle = () => {
+    if (nameEditActive === false) {
+      setNameEditActive(true);
+      setEmailEditActive(false);
+      setPhoneEditActive(false);
+    }
+    if (nameEditActive === true) {
+      setNameEditActive(false);
+      setEmailEditActive(false);
+      setPhoneEditActive(false);
+    }
+  };
 
-    this.phoneEditActiveToggle = () => {
-      const { editActive } = this.state;
-      const { phoneEditActive } = editActive;
-      if (phoneEditActive === false) {
-        this.setState({
-          editActive: {
-            nameEditActive: false,
-            emailEditActive: false,
-            phoneEditActive: true,
-          },
-        });
-      }
-      if (phoneEditActive === true) {
-        this.setState({
-          editActive: {
-            nameEditActive: false,
-            emailEditActive: false,
-            phoneEditActive: false,
-          },
-        });
-      }
-    };
-  }
+  const emailEditActiveToggle = () => {
+    if (emailEditActive === false) {
+      setNameEditActive(false);
+      setEmailEditActive(true);
+      setPhoneEditActive(false);
+    }
+    if (emailEditActive === true) {
+      setNameEditActive(false);
+      setEmailEditActive(false);
+      setPhoneEditActive(false);
+    }
+  };
 
-  render() {
-    const { submitted } = this.state;
-    const { name } = this.state;
-    const { email } = this.state;
-    const { phone } = this.state;
-    const { editActive } = this.state;
-    const { nameEditActive } = editActive;
-    const { emailEditActive } = editActive;
-    const { phoneEditActive } = editActive;
-    return (
-      <div className="general-info">
-        <header>General Information</header>
-        <main>
-          <form
-            onSubmit={(e) => {
-              this.setState({ submitted: true });
-              e.preventDefault();
-            }}
-            onChange={this.updateStateNameEmailPhone}
-          >
-            <NameComponent
-              submitted={submitted}
-              name={name}
-              updateStateNameEmailPhone={this.updateStateNameEmailPhone}
-              nameEditActive={nameEditActive}
-              nameEditActiveToggle={this.nameEditActiveToggle}
-            />
-            <EmailComponent
-              submitted={submitted}
-              email={email}
-              updateStateNameEmailPhone={this.updateStateNameEmailPhone}
-              emailEditActive={emailEditActive}
-              emailEditActiveToggle={this.emailEditActiveToggle}
-            />
-            <PhoneComponent
-              submitted={submitted}
-              phone={phone}
-              updateStateNameEmailPhone={this.updateStateNameEmailPhone}
-              phoneEditActive={phoneEditActive}
-              phoneEditActiveToggle={this.phoneEditActiveToggle}
-            />
-            <SubmitComponent submitted={submitted} />
-          </form>
-        </main>
-      </div>
-    );
-  }
+  const phoneEditActiveToggle = () => {
+    if (phoneEditActive === false) {
+      setNameEditActive(false);
+      setEmailEditActive(false);
+      setPhoneEditActive(true);
+    }
+    if (phoneEditActive === true) {
+      setNameEditActive(false);
+      setEmailEditActive(false);
+      setPhoneEditActive(false);
+    }
+  };
+
+  return (
+    <div className="general-info">
+      <header>General Information</header>
+      <main>
+        <form
+          onSubmit={(e) => {
+            setSubmitted(true);
+            e.preventDefault();
+          }}
+          onChange={updateStateNameEmailPhone}
+        >
+          <NameComponent
+            submitted={submitted}
+            name={name}
+            updateStateNameEmailPhone={updateStateNameEmailPhone}
+            nameEditActive={nameEditActive}
+            nameEditActiveToggle={nameEditActiveToggle}
+          />
+          <EmailComponent
+            submitted={submitted}
+            email={email}
+            updateStateNameEmailPhone={updateStateNameEmailPhone}
+            emailEditActive={emailEditActive}
+            emailEditActiveToggle={emailEditActiveToggle}
+          />
+          <PhoneComponent
+            submitted={submitted}
+            phone={phone}
+            updateStateNameEmailPhone={updateStateNameEmailPhone}
+            phoneEditActive={phoneEditActive}
+            phoneEditActiveToggle={phoneEditActiveToggle}
+          />
+          <SubmitComponent submitted={submitted} />
+        </form>
+      </main>
+    </div>
+  );
 }
 
 export default GeneralInfo;

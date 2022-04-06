1f37c744f9105f73f1fac6e0cd97726482c556d4
Alex Knott <alextknott@gmail.com>
2022-04-07 00:19:06 +0100

    Make StudyDateComponent a functional component

--- src/components/StudyDateComponent.jsx
+++ src/components/StudyDateComponent.jsx
@@ -1,67 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 
-class StudyDateComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { studyDate } = this.props;
-    const { updateStateSchoolTitleDate } = this.props;
-    const { studyDateEditActive } = this.props;
-    const { studyDateEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function StudyDateComponent(props) {
+  const { submitted } = props;
+  const { studyDate } = props;
+  const { updateStateSchoolTitleDate } = props;
+  const { studyDateEditActive } = props;
+  const { studyDateEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="study-date">
+          <label htmlFor="study-date-input">
+            Study Date
+            <input id="study-date-input" type="date" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (studyDateEditActive === false) {
         return (
           <div className="study-date">
             <label htmlFor="study-date-input">
               Study Date
-              <input id="study-date-input" type="date" />
+              <p>{studyDate}</p>
+              <button type="button" onClick={studyDateEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (studyDateEditActive === false) {
-          return (
-            <div className="study-date">
-              <label htmlFor="study-date-input">
-                Study Date
-                <p>{studyDate}</p>
-                <button type="button" onClick={studyDateEditActiveToggle}>
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (studyDateEditActive === true) {
-          return (
-            <div className="study-date">
-              <label htmlFor="study-date-input">
-                Study Date
-                <input
-                  id="study-date-input"
-                  type="date"
-                  defaultValue={studyDate}
-                />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    studyDateEditActiveToggle();
-                    updateStateSchoolTitleDate(e);
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (studyDateEditActive === true) {
+        return (
+          <div className="study-date">
+            <label htmlFor="study-date-input">
+              Study Date
+              <input
+                id="study-date-input"
+                type="date"
+                defaultValue={studyDate}
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  studyDateEditActiveToggle();
+                  updateStateSchoolTitleDate(e);
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
+    }
+    return false;
+  };
 
-    return <div>{inputOrStatic()}</div>;
-  }
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default StudyDateComponent;

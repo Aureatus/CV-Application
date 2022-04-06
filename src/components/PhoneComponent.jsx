17e8fd538a528ab865e118ab54ff8b757d7af7cf
Alex Knott <alextknott@gmail.com>
2022-04-06 21:53:54 +0100

    Make PhoneComponent a functional component

--- src/components/PhoneComponent.jsx
+++ src/components/PhoneComponent.jsx
@@ -1,70 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 
-class PhoneComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { phone } = this.props;
-    const { updateStateNameEmailPhone } = this.props;
-    const { phoneEditActive } = this.props;
-    const { phoneEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function PhoneComponent(props) {
+  const { submitted } = props;
+  const { phone } = props;
+  const { updateStateNameEmailPhone } = props;
+  const { phoneEditActive } = props;
+  const { phoneEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="phone-number">
+          <label htmlFor="phone-number-input">
+            Phone Number
+            <input id="phone-number-input" type="tel" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (phoneEditActive === false) {
         return (
-          <div className="phone-number">
-            <label htmlFor="phone-number-input">
+          <div className="phone">
+            <label htmlFor="phone-input">
               Phone Number
-              <input id="phone-number-input" type="tel" />
+              <p>{phone}</p>
+              <button type="button" onClick={phoneEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (phoneEditActive === false) {
-          return (
-            <div className="phone">
-              <label htmlFor="phone-input">
-                Phone Number
-                <p>{phone}</p>
-                <button type="button" onClick={phoneEditActiveToggle}>
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (phoneEditActive === true) {
-          return (
-            <div className="phone">
-              <label htmlFor="phone-number-input">
-                Phone Number
-                <input
-                  id="phone-number-input"
-                  type="phone"
-                  defaultValue={phone}
-                  required
-                />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    const phoneInput = e.target.parentElement.children[0];
-                    if (phoneInput.validity.valid) {
-                      phoneEditActiveToggle();
-                      updateStateNameEmailPhone(e);
-                    }
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (phoneEditActive === true) {
+        return (
+          <div className="phone">
+            <label htmlFor="phone-number-input">
+              Phone Number
+              <input
+                id="phone-number-input"
+                type="phone"
+                defaultValue={phone}
+                required
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  const phoneInput = e.target.parentElement.children[0];
+                  if (phoneInput.validity.valid) {
+                    phoneEditActiveToggle();
+                    updateStateNameEmailPhone(e);
+                  }
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
-    return <div>{inputOrStatic()}</div>;
-  }
+    }
+    return false;
+  };
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default PhoneComponent;

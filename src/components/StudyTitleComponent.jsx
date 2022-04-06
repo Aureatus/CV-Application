3186c8263a81a323950f6b1a7bb656b711fc5443
Alex Knott <alextknott@gmail.com>
2022-04-07 00:17:34 +0100

    Make StudyTitleComponent a functional component

--- src/components/StudyTitleComponent.jsx
+++ src/components/StudyTitleComponent.jsx
@@ -1,67 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 
-class StudyTitleComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { studyTitle } = this.props;
-    const { updateStateSchoolTitleDate } = this.props;
-    const { studyTitleEditActive } = this.props;
-    const { studyTitleEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function StudyTitleComponent(props) {
+  const { submitted } = props;
+  const { studyTitle } = props;
+  const { updateStateSchoolTitleDate } = props;
+  const { studyTitleEditActive } = props;
+  const { studyTitleEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="study-title">
+          <label htmlFor="study-title-input">
+            Study Title
+            <input id="study-title-input" type="text" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (studyTitleEditActive === false) {
         return (
           <div className="study-title">
             <label htmlFor="study-title-input">
               Study Title
-              <input id="study-title-input" type="text" />
+              <p>{studyTitle}</p>
+              <button type="button" onClick={studyTitleEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (studyTitleEditActive === false) {
-          return (
-            <div className="study-title">
-              <label htmlFor="study-title-input">
-                Study Title
-                <p>{studyTitle}</p>
-                <button type="button" onClick={studyTitleEditActiveToggle}>
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (studyTitleEditActive === true) {
-          return (
-            <div className="study-title">
-              <label htmlFor="study-title-input">
-                Study Title
-                <input
-                  id="study-title-input"
-                  type="text"
-                  defaultValue={studyTitle}
-                />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    studyTitleEditActiveToggle();
-                    updateStateSchoolTitleDate(e);
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (studyTitleEditActive === true) {
+        return (
+          <div className="study-title">
+            <label htmlFor="study-title-input">
+              Study Title
+              <input
+                id="study-title-input"
+                type="text"
+                defaultValue={studyTitle}
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  studyTitleEditActiveToggle();
+                  updateStateSchoolTitleDate(e);
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
+    }
+    return false;
+  };
 
-    return <div>{inputOrStatic()}</div>;
-  }
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default StudyTitleComponent;

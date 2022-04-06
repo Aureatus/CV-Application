af2d02b14d007fa3b7c47e370164824fc100342e
Alex Knott <alextknott@gmail.com>
2022-04-06 21:44:39 +0100

    Make NameComponent a function component

--- src/components/NameComponent.jsx
+++ src/components/NameComponent.jsx
@@ -1,63 +1,61 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NameComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { name } = this.props;
-    const { updateStateNameEmailPhone } = this.props;
-    const { nameEditActive } = this.props;
-    const { nameEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function NameComponent(props) {
+  const { submitted } = props;
+  const { name } = props;
+  const { updateStateNameEmailPhone } = props;
+  const { nameEditActive } = props;
+  const { nameEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="name">
+          <label htmlFor="name-input">
+            Name
+            <input id="name-input" type="text" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (nameEditActive === false) {
         return (
           <div className="name">
             <label htmlFor="name-input">
               Name
-              <input id="name-input" type="text" />
+              <p>{name}</p>
+              <button type="button" onClick={nameEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (nameEditActive === false) {
-          return (
-            <div className="name">
-              <label htmlFor="name-input">
-                Name
-                <p>{name}</p>
-                <button type="button" onClick={nameEditActiveToggle}>
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (nameEditActive === true) {
-          return (
-            <div className="name">
-              <label htmlFor="name-input">
-                Name
-                <input id="name-input" type="text" defaultValue={name} />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    nameEditActiveToggle();
-                    updateStateNameEmailPhone(e);
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (nameEditActive === true) {
+        return (
+          <div className="name">
+            <label htmlFor="name-input">
+              Name
+              <input id="name-input" type="text" defaultValue={name} />
+              <button
+                type="button"
+                onClick={(e) => {
+                  nameEditActiveToggle();
+                  updateStateNameEmailPhone(e);
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
+    }
+    return false;
+  };
 
-    return <div>{inputOrStatic()}</div>;
-  }
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default NameComponent;

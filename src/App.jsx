fa981ea58fbe8abc3868180cb82d651ef7cf3d36
Alex Knott <alextknott@gmail.com>
2022-04-06 20:34:29 +0100

    Make App a functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import GeneralInfo from "./components/GeneralInfo";
 /*  import EducationalInfo from "./components/EducationalInfo";
 import PracticalInfo from "./components/PracticalInfo";
 */
 import "./styles/App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <GeneralInfo />
-        {/*  <hr />
-        <EducationalInfo />
-        <hr />
-    <PracticalInfo /> */}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <GeneralInfo />
+      {/*  <hr />
+          <EducationalInfo />
+          <hr />
+      <PracticalInfo /> */}
+    </div>
+  );
 }
 
 export default App;

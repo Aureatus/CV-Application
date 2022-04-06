bcedf859dcca3235d8005af6169978df1a722463
Alex Knott <alextknott@gmail.com>
2022-04-07 00:00:21 +0100

    Make SchoolName a functional component

--- src/components/SchoolNameComponent.jsx
+++ src/components/SchoolNameComponent.jsx
@@ -1,67 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 
-class SchoolNameComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { schoolName } = this.props;
-    const { updateStateSchoolTitleDate } = this.props;
-    const { schoolNameEditActive } = this.props;
-    const { schoolNameEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function SchoolNameComponent(props) {
+  const { submitted } = props;
+  const { schoolName } = props;
+  const { updateStateSchoolTitleDate } = props;
+  const { schoolNameEditActive } = props;
+  const { schoolNameEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="school-name">
+          <label htmlFor="school-name-input">
+            School Name
+            <input id="school-name-input" type="text" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (schoolNameEditActive === false) {
         return (
           <div className="school-name">
             <label htmlFor="school-name-input">
               School Name
-              <input id="school-name-input" type="text" />
+              <p>{schoolName}</p>
+              <button type="button" onClick={schoolNameEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (schoolNameEditActive === false) {
-          return (
-            <div className="school-name">
-              <label htmlFor="school-name-input">
-                School Name
-                <p>{schoolName}</p>
-                <button type="button" onClick={schoolNameEditActiveToggle}>
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (schoolNameEditActive === true) {
-          return (
-            <div className="school-name">
-              <label htmlFor="school-name-input">
-                School Name
-                <input
-                  id="school-name-input"
-                  type="text"
-                  defaultValue={schoolName}
-                />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    schoolNameEditActiveToggle();
-                    updateStateSchoolTitleDate(e);
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (schoolNameEditActive === true) {
+        return (
+          <div className="school-name">
+            <label htmlFor="school-name-input">
+              School Name
+              <input
+                id="school-name-input"
+                type="text"
+                defaultValue={schoolName}
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  schoolNameEditActiveToggle();
+                  updateStateSchoolTitleDate(e);
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
+    }
+    return false;
+  };
 
-    return <div>{inputOrStatic()}</div>;
-  }
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default SchoolNameComponent;

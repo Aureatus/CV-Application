96a5aae57d6adf3e2f02cf173af3298cd43a344e
Alex Knott <alextknott@gmail.com>
2022-04-06 21:56:44 +0100

    Uncomment EducationalInfo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import GeneralInfo from "./components/GeneralInfo";
-/*  import EducationalInfo from "./components/EducationalInfo";
-import PracticalInfo from "./components/PracticalInfo";
-*/
+import EducationalInfo from "./components/EducationalInfo";
+/* import PracticalInfo from "./components/PracticalInfo";
+ */
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="app">
       <GeneralInfo />
-      {/*  <hr />
-          <EducationalInfo />
-          <hr />
-      <PracticalInfo /> */}
+      <hr />
+      <EducationalInfo />
+      <hr />
+      {/* <PracticalInfo /> */}
     </div>
   );
 }

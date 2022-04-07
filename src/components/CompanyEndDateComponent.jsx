19c3f1a89af253206f3a3ae5613db8533ea1054e
Alex Knott <alextknott@gmail.com>
2022-04-07 01:33:46 +0100

    Make CompanyEndDateComponent a functional component

--- src/components/CompanyEndDateComponent.jsx
+++ src/components/CompanyEndDateComponent.jsx
@@ -1,67 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 
-class CompanyEndDateComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { companyEndDate } = this.props;
-    const { updateStateNameTitleTasksDate } = this.props;
-    const { companyEndDateEditActive } = this.props;
-    const { companyEndDateEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function CompanyEndDateComponent(props) {
+  const { submitted } = props;
+  const { companyEndDate } = props;
+  const { updateStateNameTitleTasksDate } = props;
+  const { companyEndDateEditActive } = props;
+  const { companyEndDateEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="company-end-date">
+          <label htmlFor="company-end-date-input">
+            Company End Date
+            <input id="company-end-date-input" type="date" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (companyEndDateEditActive === false) {
         return (
           <div className="company-end-date">
             <label htmlFor="company-end-date-input">
               Company End Date
-              <input id="company-end-date-input" type="date" />
+              <p>{companyEndDate}</p>
+              <button type="button" onClick={companyEndDateEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (companyEndDateEditActive === false) {
-          return (
-            <div className="company-end-date">
-              <label htmlFor="company-end-date-input">
-                Company End Date
-                <p>{companyEndDate}</p>
-                <button type="button" onClick={companyEndDateEditActiveToggle}>
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (companyEndDateEditActive === true) {
-          return (
-            <div className="company-end-date">
-              <label htmlFor="company-end-date-input">
-                Company End Date
-                <input
-                  id="company-end-date-input"
-                  type="date"
-                  defaultValue={companyEndDate}
-                />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    companyEndDateEditActiveToggle();
-                    updateStateNameTitleTasksDate(e);
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (companyEndDateEditActive === true) {
+        return (
+          <div className="company-end-date">
+            <label htmlFor="company-end-date-input">
+              Company End Date
+              <input
+                id="company-end-date-input"
+                type="date"
+                defaultValue={companyEndDate}
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  companyEndDateEditActiveToggle();
+                  updateStateNameTitleTasksDate(e);
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
+    }
+    return false;
+  };
 
-    return <div>{inputOrStatic()}</div>;
-  }
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default CompanyEndDateComponent;

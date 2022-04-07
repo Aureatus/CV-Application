6bbea653ebfa559f45abe4f8c0d3bd69b6330e5f
Alex Knott <alextknott@gmail.com>
2022-04-07 01:31:51 +0100

    Make CompanyStartDateComponent a functional component

--- src/components/CompanyStartDateComponent.jsx
+++ src/components/CompanyStartDateComponent.jsx
@@ -1,70 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 
-class CompanyStartDateComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { companyStartDate } = this.props;
-    const { updateStateNameTitleTasksDate } = this.props;
-    const { companyStartDateEditActive } = this.props;
-    const { companyStartDateEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function CompanyStartDateComponent(props) {
+  const { submitted } = props;
+  const { companyStartDate } = props;
+  const { updateStateNameTitleTasksDate } = props;
+  const { companyStartDateEditActive } = props;
+  const { companyStartDateEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="company-start-date">
+          <label htmlFor="company-start-date-input">
+            Company Start Date
+            <input id="company-start-date-input" type="date" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (companyStartDateEditActive === false) {
         return (
           <div className="company-start-date">
             <label htmlFor="company-start-date-input">
               Company Start Date
-              <input id="company-start-date-input" type="date" />
+              <p>{companyStartDate}</p>
+              <button type="button" onClick={companyStartDateEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (companyStartDateEditActive === false) {
-          return (
-            <div className="company-start-date">
-              <label htmlFor="company-start-date-input">
-                Company Start Date
-                <p>{companyStartDate}</p>
-                <button
-                  type="button"
-                  onClick={companyStartDateEditActiveToggle}
-                >
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (companyStartDateEditActive === true) {
-          return (
-            <div className="company-start-date">
-              <label htmlFor="company-start-date-input">
-                Company Start Date
-                <input
-                  id="company-start-date-input"
-                  type="date"
-                  defaultValue={companyStartDate}
-                />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    companyStartDateEditActiveToggle();
-                    updateStateNameTitleTasksDate(e);
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (companyStartDateEditActive === true) {
+        return (
+          <div className="company-start-date">
+            <label htmlFor="company-start-date-input">
+              Company Start Date
+              <input
+                id="company-start-date-input"
+                type="date"
+                defaultValue={companyStartDate}
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  companyStartDateEditActiveToggle();
+                  updateStateNameTitleTasksDate(e);
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
+    }
+    return false;
+  };
 
-    return <div>{inputOrStatic()}</div>;
-  }
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default CompanyStartDateComponent;

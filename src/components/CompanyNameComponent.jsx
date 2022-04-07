e3a98a7c142a3d743e736661499e9158e0ed9a13
Alex Knott <alextknott@gmail.com>
2022-04-07 01:27:18 +0100

    Make CompanyNameComponent a functional component

--- src/components/CompanyNameComponent.jsx
+++ src/components/CompanyNameComponent.jsx
@@ -1,67 +1,65 @@
-import React, { Component } from "react";
+import React from "react";
 
-class CompanyNameComponent extends Component {
-  render() {
-    const { submitted } = this.props;
-    const { companyName } = this.props;
-    const { updateStateNameTitleTasksDate } = this.props;
-    const { companyNameEditActive } = this.props;
-    const { companyNameEditActiveToggle } = this.props;
-    const inputOrStatic = () => {
-      if (submitted === false) {
+function CompanyNameComponent(props) {
+  const { submitted } = props;
+  const { companyName } = props;
+  const { updateStateNameTitleTasksDate } = props;
+  const { companyNameEditActive } = props;
+  const { companyNameEditActiveToggle } = props;
+  const inputOrStatic = () => {
+    if (submitted === false) {
+      return (
+        <div className="company-name">
+          <label htmlFor="company-name-input">
+            Company Name
+            <input id="company-name-input" type="text" />
+          </label>
+        </div>
+      );
+    }
+
+    if (submitted === true) {
+      if (companyNameEditActive === false) {
         return (
           <div className="company-name">
             <label htmlFor="company-name-input">
               Company Name
-              <input id="company-name-input" type="text" />
+              <p>{companyName}</p>
+              <button type="button" onClick={companyNameEditActiveToggle}>
+                Edit
+              </button>
             </label>
           </div>
         );
       }
-
-      if (submitted === true) {
-        if (companyNameEditActive === false) {
-          return (
-            <div className="company-name">
-              <label htmlFor="company-name-input">
-                Company Name
-                <p>{companyName}</p>
-                <button type="button" onClick={companyNameEditActiveToggle}>
-                  Edit
-                </button>
-              </label>
-            </div>
-          );
-        }
-        if (companyNameEditActive === true) {
-          return (
-            <div className="company-name">
-              <label htmlFor="company-name-input">
-                Company Name
-                <input
-                  id="company-name-input"
-                  type="text"
-                  defaultValue={companyName}
-                />
-                <button
-                  type="button"
-                  onClick={(e) => {
-                    companyNameEditActiveToggle();
-                    updateStateNameTitleTasksDate(e);
-                  }}
-                >
-                  Submit
-                </button>
-              </label>
-            </div>
-          );
-        }
+      if (companyNameEditActive === true) {
+        return (
+          <div className="company-name">
+            <label htmlFor="company-name-input">
+              Company Name
+              <input
+                id="company-name-input"
+                type="text"
+                defaultValue={companyName}
+              />
+              <button
+                type="button"
+                onClick={(e) => {
+                  companyNameEditActiveToggle();
+                  updateStateNameTitleTasksDate(e);
+                }}
+              >
+                Submit
+              </button>
+            </label>
+          </div>
+        );
       }
-      return false;
-    };
+    }
+    return false;
+  };
 
-    return <div>{inputOrStatic()}</div>;
-  }
+  return <div>{inputOrStatic()}</div>;
 }
 
 export default CompanyNameComponent;
